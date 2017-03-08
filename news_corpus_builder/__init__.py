eb3f04eb99ebd81dd45d92d3a098ee30d4a07c4e
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2017-03-08 23:00:19 +0700

    Update __init__.py

--- news_corpus_builder/__init__.py
+++ news_corpus_builder/__init__.py
@@ -1,2 +1,3 @@
-from news_corpus_generator import NewsCorpusGenerator
+from __future__ import absolute_import
+from news_corpus_builder.news_corpus_generator import NewsCorpusGenerator
 #from news_corpus_builder.news_corpus_generator import NewsCorpusGenerator
